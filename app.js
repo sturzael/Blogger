06ac12cd03eeed3681ef891b83435c05eb5022e1
Massey Isa'ako <lasiisaako@gmail.com>
2017-09-04 10:51:27 +1200

    updating master

--- app.js
+++ app.js
@@ -3,10 +3,6 @@ var app = express();
 var cors = require('cors');
 var path = require("path");
 
-
-
-
-
 app.use(cors());
 
 app.use(function(request, response, next){
@@ -15,10 +11,16 @@ app.use(function(request, response, next){
 })
 
 app.use(express.static("./public"));
+
 // Linking up config 
 app.use('/data', express.static(path.join(__dirname, 'data')));
 
+// jquery
+app.use("/js", express.static(path.join(__dirname, "node_modules/jquery/dist")));
 
+// bootstrap js and css
+app.use("/js", express.static(path.join(__dirname, "node_modules/bootstrap/dist/js")));
+app.use("/css", express.static(path.join(__dirname, "node_modules/bootstrap/dist/css")));
 
 app.listen(3000);
 
