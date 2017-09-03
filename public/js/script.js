70e016cf076a6a2aa19b745fea6e8568c679a21d
Sturzael <esturz23@gmail.com>
2017-09-04 10:44:44 +1200

    new function, got blog posts

--- public/js/script.js
+++ public/js/script.js
@@ -15,7 +15,7 @@ $.ajax({
     key = DataFromJson.key;
     console.log(key);
 
-getStuff();
+getAllBlog();
 
 
   },
@@ -26,9 +26,27 @@ getStuff();
 
 });
 
-function getStuff() {
+function getAllBlog() {
   $.ajax({
     url: "https://www.googleapis.com/blogger/v3/blogs/"+blogId+"?key=" + key,
+    dataType: "jsonp",
+    success: function(DataFromBlog) {
+
+      console.log(DataFromBlog);
+
+      getPosts()
+    },
+    error: function() {
+      console.log("Something went wrong");
+    }
+  });
+
+}
+
+function getPosts(){
+
+  $.ajax({
+    url: "https://www.googleapis.com/blogger/v3/blogs/"+blogId+"/posts?key=" + key,
     dataType: "jsonp",
     success: function(DataFromBlog) {
 
@@ -40,5 +58,25 @@ function getStuff() {
       console.log("Something went wrong");
     }
   });
+
+}
+
+
+function createPost(){
+  $.ajax({
+    url: "https://www.googleapis.com/blogger/v3/blogs/"+blogId+"/posts/",
+    dataType: "jsonp",
+    success: function(DataFromBlog) {
+
+      console.log(DataFromBlog);
+
+
+    },
+    error: function() {
+      console.log("Something went wrong");
+    }
+  });
+
+
 
 }
