9ff6b8e588fb3d90462d7cd15dbe4d691e8ae6b8
Massey Isa'ako <lasiisaako@gmail.com>
2017-09-06 10:41:22 +1200

    trying to pull url bar

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var app = express();
 var cors = require('cors');
 var path = require("path");
 var config = require("./data/config.json");
+var readline = require("readline");
 
 app.use(cors());
 
@@ -34,8 +35,55 @@ app.post("/sendMessage=:message", function(request, response){
 	console.log(message);
 });
 
-app.listen(3000);
+var google = require('googleapis');
+var OAuth2 = google.auth.OAuth2;
+var plus = google.plus('v1');
 
-console.log(config.apiKey);
+var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+var oauth2Client = new OAuth2(
+ config.clientId,
+ config.clientSecret,
+ "http://localhost:3000"
+);
+
+function getAccessToken (oauth2Client, callback) {
+ // generate consent page url
+ var url = oauth2Client.generateAuthUrl({
+   access_type: 'online', // will return a refresh token
+   scope: 'https://www.googleapis.com/auth/blogger' // can be a space-delimited string or an array of scopes
+ });
+
+ console.log('Visit the url: ', url);
+ rl.question('Enter the code here:', function (code) {
+   // request access token
+
+   oauth2Client.getToken(code, function (err, tokens) {
+     if (err) {
+       return callback(err);
+     }
+     // set tokens to the client
+     // TODO: tokens should be set by OAuth2 client.
+     oauth2Client.setCredentials(tokens);
+     callback();
+   });
+ });
+}
+
+// retrieve an access token
+getAccessToken(oauth2Client, function () {
+	// retrieve user profile
+	plus.people.get({ userId: 'me', auth: oauth2Client }, function (err, profile) {
+		if (err) {
+		return console.log('An error occured', err);
+		}
+		console.log(profile.displayName, ':', profile.tagline);
+	});
+});
+
+app.listen(3000);
 
 console.log("server running on port 3000");
--- public/js/script.js
+++ public/js/script.js
@@ -13,7 +13,7 @@ $.ajax({
     }
 
   },
-    success: function(DataFromJson) {
+    success: function(DataFromJson){
 
       keyList.push({
         apiKey: DataFromJson.apiKey,
@@ -24,17 +24,9 @@ $.ajax({
 
       key = keyList[0].apiKey;
       blogId = keyList[0].blogId;
-      clientId: DataFromJson.clientId,
-      clientSecret: DataFromJson.clientSecret
 
-    })
-
-    // getStuff();
-
-    console.log(blogId);
-
-    // getAllBlog();
-
+      // getStuff();
+      // getAllBlog();
   },
   error: function() {
     console.log("Something Went Wrong");
@@ -108,116 +100,116 @@ function getPosts(){
 
 // Getting Authentication for Google Blogger
 // https://www.googleapis.com/auth/blogger
-var discoveryDocs = ["https://people.googleapis.com/$discovery/rest?version=v1"];
-var authorizeButton = document.getElementById('authorize-button');
-var signoutButton = document.getElementById('signout-button');
+// var discoveryDocs = ["https://people.googleapis.com/$discovery/rest?version=v1"];
+// var authorizeButton = document.getElementById('authorize-button');
+// var signoutButton = document.getElementById('signout-button');
 
-// handleClientLoad();
+// // handleClientLoad();
 
-function handleClientLoad(){
-  // Loading the Google Client JavaScript Authentication Library
-  gapi.load('client:auth2', initClient);
-}
+// function handleClientLoad(){
+//   // Loading the Google Client JavaScript Authentication Library
+//   gapi.load('client:auth2', initClient);
+// }
 
-function initClient(){
-  // Using the config keys to access the api
-  gapi.client.init({
-      apiKey: keyList[0].apiKey,
-      discoveryDocs: discoveryDocs,
-      clientId: keyList[0].clientId,
-      scope: scopes
-  }).then(function (){
-    // Checking whether user is signed in
-    gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+// function initClient(){
+//   // Using the config keys to access the api
+//   gapi.client.init({
+//       apiKey: keyList[0].apiKey,
+//       discoveryDocs: discoveryDocs,
+//       clientId: keyList[0].clientId,
+//       scope: scopes
+//   }).then(function (){
+//     // Checking whether user is signed in
+//     gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
 
-    // Handling the existing sign-in state, in case user is already signed in.
-    updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+//     // Handling the existing sign-in state, in case user is already signed in.
+//     updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
 
-    // Click function for sign in/sign out buttons
-    authorizeButton.onclick = handleAuthClick;
-    signoutButton.onclick = handleSignoutClick;
-  });
-}
+//     // Click function for sign in/sign out buttons
+//     authorizeButton.onclick = handleAuthClick;
+//     signoutButton.onclick = handleSignoutClick;
+//   });
+// }
 
-function updateSigninStatus(isSignedIn){
-  // If user is signed in, show/hide authentication sign buttons
-  if (isSignedIn) {
-    // Hide sign in, show sign out.
-    authorizeButton.style.display = 'none';
-    signoutButton.style.display = 'block';
-    makeApiCall();
-  } else {
-    // Hide sign out, show sign in.
-    authorizeButton.style.display = 'block';
-    signoutButton.style.display = 'none';
-  }
-}
+// function updateSigninStatus(isSignedIn){
+//   // If user is signed in, show/hide authentication sign buttons
+//   if (isSignedIn) {
+//     // Hide sign in, show sign out.
+//     authorizeButton.style.display = 'none';
+//     signoutButton.style.display = 'block';
+//     makeApiCall();
+//   } else {
+//     // Hide sign out, show sign in.
+//     authorizeButton.style.display = 'block';
+//     signoutButton.style.display = 'none';
+//   }
+// }
 
-// This is where authentication is called
-function handleAuthClick(event){
-  gapi.auth2.getAuthInstance().signIn();
-}
+// // This is where authentication is called
+// function handleAuthClick(event){
+//   gapi.auth2.getAuthInstance().signIn();
+// }
 
-// This is the sign out function
-function handleSignoutClick(event){
-  gapi.auth2.getAuthInstance().signOut();
-}
+// // This is the sign out function
+// function handleSignoutClick(event){
+//   gapi.auth2.getAuthInstance().signOut();
+// }
 
-// Load the API and make an API call.  Display the results on the screen.
-function makeApiCall(){
-  gapi.client.people.people.get({
-    'resourceName': 'people/me',
-    'requestMask.includeField': 'person.names'
-  }).then(function(resp) {
-    var p = document.createElement('p');
-    var name = resp.result.names[0].givenName;
-    p.appendChild(document.createTextNode('Hello, '+name+'!'));
-    document.getElementById('content').appendChild(p);
-    console.log(resp);
-  });
-}
+// // Load the API and make an API call.  Display the results on the screen.
+// function makeApiCall(){
+//   gapi.client.people.people.get({
+//     'resourceName': 'people/me',
+//     'requestMask.includeField': 'person.names'
+//   }).then(function(resp) {
+//     var p = document.createElement('p');
+//     var name = resp.result.names[0].givenName;
+//     p.appendChild(document.createTextNode('Hello, '+name+'!'));
+//     document.getElementById('content').appendChild(p);
+//     console.log(resp);
+//   });
+// }
 
-$("#postBlog").submit(function(event){
+// $("#postBlog").submit(function(event){
 
-  // Preventing Form Submission
-  event.preventDefault();
+//   // Preventing Form Submission
+//   event.preventDefault();
 
-  // Setting variables to retrieve information
-  var title = $("#postTitle").val();
-  var message = $("#postMessage").val();
-  var url = "http://localhost:3000";
+//   // Setting variables to retrieve information
+//   var title = $("#postTitle").val();
+//   var message = $("#postMessage").val();
+//   var url = "http://localhost:3000";
 
-  url += "/sendTitle=" + title + "/sendMessage=" + message;
+//   url += "/sendTitle=" + title + "/sendMessage=" + message;
 
-  $.ajax({
-    url: url,
-    type: "post",
-    dataType: "json",
-    success:function(PostData){
-      console.log("Message: " + PostData + " was sent.");
-    }, error:function(){
-      console.log("url undefined");
-    }
-  });
-});
+//   $.ajax({
+//     url: url,
+//     type: "post",
+//     dataType: "json",
+//     success:function(PostData){
+//       console.log("Message: " + PostData + " was sent.");
+//     }, error:function(){
+//       console.log("url undefined");
+//     }
+//   });
+// });
 
-function start() {
-  // 2. Initialize the JavaScript client library.
-  gapi.client.init({
-    'apiKey': keyList[0].apiKey,
-    // clientId and scope are optional if auth is not required.
-    'clientId': keyList[0].clientId,
-    'scope': 'profile',
-  }).then(function() {
-    // 3. Initialize and make the API request.
-    return gapi.client.request({
-      'path': 'https://people.googleapis.com/v1/people/me?requestMask.includeField=person.names',
-    })
-  }).then(function(response) {
-    console.log(response.result);
-  }, function(reason) {
-    console.log('Error: ' + reason.result.error.message);
-  });
-};
+// function start() {
+//   // 2. Initialize the JavaScript client library.
+//   gapi.client.init({
+//     'apiKey': keyList[0].apiKey,
+//     // clientId and scope are optional if auth is not required.
+//     'clientId': keyList[0].clientId,
+//     'scope': 'profile',
+//   }).then(function() {
+//     // 3. Initialize and make the API request.
+//     return gapi.client.request({
+//       'path': 'https://people.googleapis.com/v1/people/me?requestMask.includeField=person.names',
+//     })
+//   }).then(function(response) {
+//     console.log(response.result);
+//   }, function(reason) {
+//     console.log('Error: ' + reason.result.error.message);
+//   });
+// };
 
- gapi.load("client", start);
+//  gapi.load("client", start);
